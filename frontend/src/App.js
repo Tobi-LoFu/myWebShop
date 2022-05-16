e155b2f49700acd6d1da05bef60623e4194e47c4
Tobias Pfaffenzeller <tobler@gmx.com>
2022-05-16 16:00:50 +0200

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">BikeShop</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
